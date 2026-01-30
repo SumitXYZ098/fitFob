fb7f894aa77a5647a5ab656b184bf3fa87cdb14f
Sumit Thakur <sumit@thexyzstudio.com>
2026-01-30 19:02:57 +0530

    create the .env file

--- app/config/firebaseConfig.ts
+++ app/config/firebaseConfig.ts
@@ -4,13 +4,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // TODO: Replace the following with your app's Firebase project configuration
  const firebaseConfig = {
-    apiKey: "AIzaSyA-4T1-DBr85gmcFhDInUBar8hfqgBsJNA",
-    authDomain: "fit-fob.firebaseapp.com",
-    projectId: "fit-fob",
-    storageBucket: "fit-fob.firebasestorage.app",
-    messagingSenderId: "1026944446347",
-    appId: "1:1026944446347:web:2645554ea8aec06b65898e",
-    measurementId: "G-KXXVJ2MWCC"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
